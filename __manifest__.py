2c43fbdfab00cddd5dc107fe93ccf853622ffaab
davidalbigarcia <davidalbigarcia@gmail.com>
2025-04-21 22:29:03 +0200

    creacion mision desde Orden trabajo operativa - Grupos y permisos añadidos (no dinámicos

--- __manifest__.py
+++ __manifest__.py
@@ -16,7 +16,7 @@
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
+    'category': 'Malkion',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
@@ -24,7 +24,8 @@
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
+        'security/groups.xml',
+        'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
         'views/malkion_dashboard_views.xml', # primero, copón!
--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 
-from . import models
 from . import malkion_contract
 from . import malkion_point_of_interest
 from . import malkion_almacenes
